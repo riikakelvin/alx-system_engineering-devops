1fd43493be44eeaecb4f2461c0f6d29fc18bd642
riikakelvin <riikakelvin2@gmail.com>
2023-09-26 15:23:28 -0700

    commit for task advanced

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Install
+exec{ 'Install':
+  command  => 'sudo apt-get update && sudo apt-get -y install nginx && echo "Hello world!" | sudo tee /var/www/html/index.nginx-debian.html && sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default-backup && redirect="\n\tserver_name riika.tech;\n\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" && sudo sed -i "37i\ $redirect" /etc/nginx/sites-available/default && sudo service nginx start',
+  provider => shell,
+}
