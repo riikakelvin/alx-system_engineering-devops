d893403b3f6a6c6d865adf7a8884cbaf406f7279
riikakelvin <riikakelvin2@gmail.com>
2023-09-26 06:08:21 -0700

    commit for corrective lines of code to meet task instructions

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-file { '/tmp':
-  path    => /tmp/school,
-  ensure  => present,
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0744',
-  content => 'I love Puppet',
+file {'/tmp/school':
+        ensure  => 'file',
+        path    => '/tmp/school',
+        mode    => '0744',
+        group   => 'www-data',
+        owner   => 'www-data',
+        content => 'I love Puppet',
 }
