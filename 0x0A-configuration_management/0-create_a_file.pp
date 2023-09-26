a8faa43cdd9b40a4436158a900987db80f717094
riikakelvin <riikakelvin2@gmail.com>
2023-09-26 06:23:51 -0700

    commit for code tweak task 0,

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 file {'/tmp/school':
-        ensure  => present,
-        path    => '/tmp/school',
-        mode    => '0744',
-        group   => 'www-data',
-        owner   => 'www-data',
-        content => 'I love Puppet',
+	ensure  => present,
+	path    => '/tmp/school',
+	mode    => '0744',
+	group   => 'www-data',
+	owner   => 'www-data',
+	content => 'I love Puppet',
 }
